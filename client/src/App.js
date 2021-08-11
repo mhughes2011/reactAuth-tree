cc0580289060ec786e9bd244eac135ec30430291
M Hughes <mhughes2011@gmail.com>
2021-08-10 18:17:22 -0700

    added data to the context and connected it to the provider

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,11 @@ import Public from './components/Public';
 import NotFound from './components/NotFound';
 import UserSignUp from './components/UserSignUp';
 import UserSignIn from './components/UserSignIn';
+import withContext from './Context';
 import UserSignOut from './components/UserSignOut';
 import Authenticated from './components/Authenticated';
+
+const UserSignUpWithContext = withContext(UserSignUp);
 
 export default () => (
   <Router>
@@ -22,7 +25,7 @@ export default () => (
         <Route exact path="/" component={Public} />
         <Route path="/authenticated" component={Authenticated} />
         <Route path="/signin" component={UserSignIn} />
-        <Route path="/signup" component={UserSignUp} />
+        <Route path="/signup" component={UserSignUpWithContext} />
         <Route path="/signout" component={UserSignOut} />
         <Route component={NotFound} />
       </Switch>
